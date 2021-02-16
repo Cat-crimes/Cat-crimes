185124a7ab37c99ed99d4f9ae6a3c4739f63e568
Jesus Rua <jesus.rua.pareja@gmail.com>
2021-02-16 11:16:34 +0100

    User Model Created

--- models/User.js
+++ models/User.js
@@ -1,1 +1,29 @@
-// User model
+const Mongoose = require ('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema ({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i, 'Please fill a valid email format.']
+  }
+}, {
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc._id;
+      delete ret._id;
+      delete ret.__v;
+      delete ret.createdAt;
+      delete ret.password;
+      return ret;
+    }
+  }
+})
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
