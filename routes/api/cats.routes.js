b7545705567334c22435d21ec5b6a756081c4a57
Jesus Rua <jesus.rua.pareja@gmail.com>
2021-02-16 14:15:51 +0100

    cat routes

--- routes/api/cats.routes.js
+++ routes/api/cats.routes.js
@@ -1,1 +1,54 @@
 // * CATS CRUD
+const express   = require('express');
+const {restart} = require('nodemon');
+const Cat       = require('../../models/Cat');
+const router    = express.Router();
+
+router.get('/', (req, res, next) => {
+  Cat.find()
+  .then(cats => res.status(200).json(cats))
+  .catch(err => res.status(500).json(err))
+})
+
+router.get('/:id', (req, res, next) => {
+  const {id} = req.params;
+  Cat.findOne({id})
+  .then(cat => res.status(200).json(cat))
+  .catch(err => res.status(500).json(err))
+})
+
+router.post('/', (req, res, next) => {
+  // const {blueEyes, hair, whitePaw, hasBow, hasBell} = attributes
+  const { name, age, race, height, weight, blueEyes, hair, whitePaw, hasBow, hasBell } = req.body;
+  console.log(req.body)
+
+
+
+  // if(!name || !age || !race || !height || !weight || !blueEyes || !hair || !whitePaw || !hasBow || !hasBell ) {
+  //   return res.status(400).json({ message: "All fields must be complete"})
+  // }
+
+  const newCat = new Cat({
+    name,
+    age,
+    race,
+    height,
+    weight, 
+    blueEyes, 
+    hair, 
+    whitePaw, 
+    hasBow, 
+    hasBell
+    // user: req.user
+  })
+
+  newCat.save()
+  .then(cat => {
+    return res.status(200).json(cat)
+  })
+  .catch(err => res.status(500).json(err))
+})
+
+
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
 router.use('/auth', require('./auth.routes'));
-// router.use('/cats', require('./cats.routes'));
+router.use('/cats', require('./cats.routes'));
 
 module.exports = router;
