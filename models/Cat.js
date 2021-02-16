439bb4397a89ad6836738a7414fa6589b4bc4384
albacardona <alba.cardonajimenez@gmail.com>
2021-02-16 12:04:18 +0100

    signup route done

--- models/Cat.js
+++ models/Cat.js
@@ -1,9 +1,5 @@
-const { Mongoose } = require("mongoose");
-
-// TODO model
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 
 
 const catSchema = new Schema ({
--- routes/api/auth.routes.js
+++ routes/api/auth.routes.js
@@ -7,8 +7,39 @@ const User = require('../../models/User');
 const bcrypt = require('bcryptjs');
 const bcryptSalt = 10;
 
-router.post('/signup', (req, res, next) => {
-  // Signup implementation
+router.post('/signup', (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (password.length <= 5) {
+    return res.status(400).json({ message: 'Please, make your password at least 6 characters long.' })
+  }
+
+  if(!username || !email) {
+    return res.status(400).json({ message: 'Please, fill all the fields.' })
+  }
+
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ message: 'User already exists.' })
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        email,
+        password: hashPass
+      })
+
+      newUser.save()
+        .then(user => res.status(200).json(user))
+        .catch(err => res.status(500).json(err))
+    })
+
+    .catch(err => res.status(500).json(err))
+
 });
 
 router.post('/login', (req, res, next) => {
